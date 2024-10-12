cb1b8701dbd24a9c83851f0948799d80cce7cd4a
SarimSheikh1 <sarimhatif1@gmail.com>
2024-10-12 12:06:50 +0500

    Add files via upload

--- slideshow.py
+++ slideshow.py
@@ -5,24 +5,23 @@ from time import sleep
 # Title of the app
 st.title("Image Slideshow")
 
-# List of image paths
-image_paths = [
-    r"C:\Users\sarim\OneDrive\Desktop\Sarim important\hairstyle1.jpg",
-    r"C:\Users\sarim\OneDrive\Desktop\Sarim important\IMG-20240922-WA0004.jpg",
-    r"C:\Users\sarim\OneDrive\Desktop\Sarim important\IMG-20240922-WA0001.jpg",
-    r"C:\Users\sarim\OneDrive\Desktop\Sarim important\IMG-20240922-WA0007.jpg",
-    r"C:\Users\sarim\OneDrive\Desktop\Sarim important\IMG-20240922-WA0006.jpg"
-]
+# Image uploader for user to upload multiple images
+uploaded_files = st.file_uploader("Upload Images", type=['jpg', 'png', 'jpeg'], accept_multiple_files=True)
 
-image_size = (1080, 1080)
-
-# Resize images
-images = [Image.open(path).resize(image_size) for path in image_paths]
-
-# Button to start slideshow
-if st.button('Start Slideshow'):
-    for image in images:
-        # Display image
-        st.image(image)
-        # Pause for 5 seconds between images
-        sleep(5)
+image_size = (245, 500)
+# Process uploaded images
+if uploaded_files:
+    images = [Image.open(uploaded_file).resize(image_size) for uploaded_file in uploaded_files]
+    
+    # Button to start slideshow
+    if st.button('Start Slideshow'):
+        # Create a placeholder to hold the image that will change
+        image_placeholder = st.empty()
+        
+        for image in images:
+            # Display image (replacing the previous one)
+            image_placeholder.image(image)
+            # Pause for 5 seconds between images
+            sleep(5)
+else:
+    st.write("Please upload some images to start the slideshow.")
