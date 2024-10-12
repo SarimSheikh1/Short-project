12b54b9c43f91b8f0bb7269111ab8cd9449b0f60
SarimSheikh1 <sarimhatif1@gmail.com>
2024-10-12 11:50:05 +0500

    Add files via upload

--- slideshow.py
+++ slideshow.py
@@ -1,20 +1,28 @@
-from itertools import cycle
-from PIL import Image, ImageDraw
+import streamlit as st
+from PIL import Image
 from time import sleep
-import tkinter as tk
 
-root=tk.Tk()
-root.title("Slide Show")
+# Title of the app
+st.title("Image Slideshow")
 
-
+# List of image paths
 image_paths = [
     r"C:\Users\sarim\OneDrive\Desktop\Sarim important\hairstyle1.jpg",
     r"C:\Users\sarim\OneDrive\Desktop\Sarim important\IMG-20240922-WA0004.jpg",
     r"C:\Users\sarim\OneDrive\Desktop\Sarim important\IMG-20240922-WA0001.jpg",
-    r"C:\Users\sarim\OneDrive\Desktop\Sarim important\IMG-20240922-WA0007.jpg"
+    r"C:\Users\sarim\OneDrive\Desktop\Sarim important\IMG-20240922-WA0007.jpg",
     r"C:\Users\sarim\OneDrive\Desktop\Sarim important\IMG-20240922-WA0006.jpg"
 ]
 
+image_size = (1080, 1080)
 
-image_sizes=(1080,1080)
-images=[Image.open(path). resize(image_sizes) for path in image_paths]
+# Resize images
+images = [Image.open(path).resize(image_size) for path in image_paths]
+
+# Button to start slideshow
+if st.button('Start Slideshow'):
+    for image in images:
+        # Display image
+        st.image(image)
+        # Pause for 5 seconds between images
+        sleep(5)
